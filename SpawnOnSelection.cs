3db16dfa2edc7eaea1619d60fb0d3db91260f095
Aiden-ytarame <116085315+Aiden-ytarame@users.noreply.github.com>
2024-07-05 13:37:32 -0300

    added camera option

--- SpawnOnSelection.cs
+++ SpawnOnSelection.cs
@@ -13,30 +13,41 @@ public class ObjectManager_Patch
     public static bool QuickSpawn;
     public static bool ShouldSpawnOnSelec;
     public static bool ShouldFollowRot;
+    public static bool ShouldSpawnOnCamera;
     
     
     [HarmonyPatch(nameof(ObjectManager.HandlePrefabOffsetKeyframes))]
     [HarmonyPrefix]
     static void PostSetup(ref ObjectManager __instance, int _i)
     {
-        if (!ShouldSpawnOnSelec || !QuickSpawn)
+        if ((!ShouldSpawnOnSelec && !ShouldSpawnOnCamera) || !QuickSpawn)
             return;
 
+        string id = DataManager.inst.gameData.beatmapObjects[_i].prefabInstanceID;
+            
+        //so this gets the PrefabObject from the BeatmapGameObject, im unsure if this is needed,
+        //as BeatmapGameObject contains an event array but doesnt have the GetEvent() function
+        var obj = DataManager.inst.gameData.prefabObjects.Find(
+            DelegateSupport.ConvertDelegate<Il2CppSystem.Predicate<DataManager.GameData.PrefabObject>>
+                (new Predicate<DataManager.GameData.PrefabObject>(x => x.ID == id)));
+
+        if (obj == null)
+        {
+            QuickSpawn = false;
+            return;
+        }
+
+        if (ShouldSpawnOnCamera)
+        {
+            obj.GetEvent().SetVal(0, EventManager.inst.CamPos.x); //pos x
+            obj.GetEvent().SetVal(1, EventManager.inst.CamPos.y); //pos y
+            return;
+        }
+        
         var selectedObject = ObjectEditor.Inst.MainSelectedObject;
         if (ObjectManager.inst.beatmapGameObjects.TryGetValue(selectedObject.ID, out var goRef))
         {
             if (!goRef.VisualObject)
-                return;
-            
-            string id = DataManager.inst.gameData.beatmapObjects[_i].prefabInstanceID;
-            
-            //so this gets the PrefabObject from the BeatmapGameObject, im unsure if this is needed,
-            //as BeatmapGameObject contains an event array but doesnt have the GetEvent() function
-            var obj = DataManager.inst.gameData.prefabObjects.Find(
-                DelegateSupport.ConvertDelegate<Il2CppSystem.Predicate<DataManager.GameData.PrefabObject>>
-                    (new Predicate<DataManager.GameData.PrefabObject>(x => x.ID == id)));
-
-            if (obj == null)
                 return;
             
             obj.GetEvent().SetVal(0, goRef.VisualObject.position.x); //pos x
@@ -101,7 +112,7 @@ public class QS_Editor_Patch
         var tooltip = activeObj.GetComponent<TooltipTrigger>();
         tooltip.keyCodeTips.Clear();
         tooltip.title = "Spawn On Selection";
-        tooltip.description = "if an quick spawned prefab should spawn on the position of the current selection";
+        tooltip.description = "if an quick spawned prefab should spawn on the position of the current selection \nDoes not work when the Camera toggle is ON";
         activeObj.SetSiblingIndex(2);
         
         
@@ -115,8 +126,22 @@ public class QS_Editor_Patch
         tooltip = activeObj.GetComponent<TooltipTrigger>();
         tooltip.keyCodeTips.Clear();
         tooltip.title = "Follow Rotation";
-        tooltip.description = "if an quick spawned prefab should copy the rotation of the current selection";
+        tooltip.description = "if an quick spawned prefab should copy the rotation of the current selection \nDoes not work when the Camera toggle is ON";
         activeObj.SetSiblingIndex(3);
+        
+        //the Spawn on Camera toggle
+        activeObj = Object.Instantiate(active, settings);
+        activeObj.GetComponent<Toggle>().Set(false);
+        activeObj.GetComponent<Toggle>().onValueChanged.AddListener(new Action<bool>(x => {ObjectManager_Patch.ShouldSpawnOnCamera = x;}));
+        activeObj.GetChild(1).GetComponentInChildren<TextMeshProUGUI>().text = "Camera";
+
+        //setting up the tooltip
+        tooltip = activeObj.GetComponent<TooltipTrigger>();
+        tooltip.keyCodeTips.Clear();
+        tooltip.title = "Spawn on Camera";
+        tooltip.description = "if an quick spawned prefab should spawn on the center of the camera";
+        activeObj.SetSiblingIndex(4);
+
         
         
     }
