e1c25ebee36bedd4cbe573d74e10b1c218d13b4a
Aiden-ytarame <116085315+Aiden-ytarame@users.noreply.github.com>
2024-10-27 17:51:32 -0300

    added spawn pos/rot offset

--- SpawnOnSelection.cs
+++ SpawnOnSelection.cs
@@ -2,7 +2,9 @@ using System;
 using HarmonyLib;
 using Il2CppInterop.Runtime;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
+using VGFunctions;
 using Object = UnityEngine.Object;
 
 namespace PrefabOnAbsolutePos;
@@ -14,6 +16,11 @@ public class ObjectManagerPatch
     public static bool ShouldSpawnOnSelec;
     public static bool ShouldSpawnOnCamera;
     public static bool ShouldFollowRot;
+    
+    public static float Xoffset;
+    public static float Yoffset;
+    public static float Zoffset;
+    
     private static readonly string ErrorMessage = "Selected Object Invalid\n[remember to expand a prefab if you're selecting it]";
     
 
@@ -41,12 +48,12 @@ public class ObjectManagerPatch
 
         if (ShouldSpawnOnCamera)
         {
-            obj.GetEvent().SetVal(0, EventManager.inst.CamPos.x); //pos x
-            obj.GetEvent().SetVal(1, EventManager.inst.CamPos.y); //pos y
+            obj.GetEvent().SetVal(0, EventManager.inst.CamPos.x + Xoffset); //pos x
+            obj.GetEvent().SetVal(1, EventManager.inst.CamPos.y + Yoffset); //pos y
 
             if (ShouldFollowRot)
             {
-                obj.GetEvent(2).SetVal(0,  EventManager.inst.camRot); //rot
+                obj.GetEvent(2).SetVal(0,  EventManager.inst.camRot + Zoffset); //rot
             }
             QuickSpawn = false;
             return;
@@ -58,12 +65,12 @@ public class ObjectManagerPatch
             if (!goRef.VisualObject)
                 return;
             
-            obj.GetEvent().SetVal(0, goRef.VisualObject.position.x); //pos x
-            obj.GetEvent().SetVal(1, goRef.VisualObject.position.y); //pos y
+            obj.GetEvent().SetVal(0, goRef.VisualObject.position.x + Xoffset); //pos x
+            obj.GetEvent().SetVal(1, goRef.VisualObject.position.y + Yoffset); //pos y
             
             if (ShouldFollowRot)
             {
-                obj.GetEvent(2).SetVal(0, goRef.VisualObject.rotation.eulerAngles.z); //rot
+                obj.GetEvent(2).SetVal(0, goRef.VisualObject.rotation.eulerAngles.z + Zoffset); //rot
             }
         }
         else
@@ -77,6 +84,12 @@ public class ObjectManagerPatch
         QuickSpawn = false;
     }
 
+    [HarmonyPatch(nameof(ObjectManager.Awake))]
+    [HarmonyPostfix]
+    static void PostAwake()
+    {
+        
+    }
 }
 
 [HarmonyPatch(typeof(PrefabQuickSpawner))]
@@ -106,9 +119,20 @@ public class QsEditorPatch
         ObjectManagerPatch.ShouldSpawnOnCamera = false;
         ObjectManagerPatch.ShouldFollowRot = false;
         ObjectManagerPatch.ShouldSpawnOnSelec = false;
-        
-        var settings = __instance.transform.parent.Find("Editor GUI/sizer/EditorDialogs/QuickSpawnDialog/Scroll View/Viewport/Content/settings");
-        
+
+        ObjectManagerPatch.Xoffset = 0;
+        ObjectManagerPatch.Yoffset = 0;
+        ObjectManagerPatch.Zoffset = 0;
+          
+        var settings =
+            __instance.transform.parent.Find(
+                "Editor GUI/sizer/EditorDialogs/QuickSpawnDialog/Scroll View/Viewport/Content/settings");
+        var offset = __instance.transform.parent.Find(
+            "Editor GUI/sizer/EditorDialogs/GameObjectDialog/data/left/Scroll View/Viewport/Content/parent_more");
+
+        var position =
+            __instance.transform.parent.Find(
+                "Editor GUI/sizer/EditorDialogs/GameObjectDialog/data/right/position/position");
         //GameObjects]
         var content = settings.Find("1/content-wrapper/content");
         var label = content.Find("keycode-label");
@@ -118,38 +142,47 @@ public class QsEditorPatch
         var labelObj = Object.Instantiate(label, settings);
         labelObj.GetComponentInChildren<TextMeshProUGUI>().text = "Spawn On Selection?";
         labelObj.SetSiblingIndex(1);
-        
+
         //the Spawn toggle
         var activeObj = Object.Instantiate(active, settings);
         activeObj.GetComponent<Toggle>().Set(false);
-        activeObj.GetComponent<Toggle>().onValueChanged.AddListener(new Action<bool>(x => {ObjectManagerPatch.ShouldSpawnOnSelec = x;}));
+        activeObj.GetComponent<Toggle>().onValueChanged.AddListener(new Action<bool>(x =>
+        {
+            ObjectManagerPatch.ShouldSpawnOnSelec = x;
+        }));
         activeObj.Find("content/text").GetComponent<TextMeshProUGUI>().text = "Spawn";
-        
+
         //setting up the tooltip
         var tooltip = activeObj.GetComponent<TooltipTrigger>();
         tooltip.keyCodeTips.Clear();
         tooltip.title = "Spawn On Selection";
-        tooltip.description = "if an quick spawned prefab should spawn on the position of the current selection \nDoes not work when the Camera toggle is ON";
+        tooltip.description =
+            "if an quick spawned prefab should spawn on the position of the current selection \nDoes not work when the Camera toggle is ON";
         activeObj.SetSiblingIndex(2);
-        
-        
+
+
         //the Rotate toggle
         activeObj = Object.Instantiate(active, settings);
         activeObj.GetComponent<Toggle>().Set(false);
-        activeObj.GetComponent<Toggle>().onValueChanged.AddListener(new Action<bool>(x => {ObjectManagerPatch.ShouldFollowRot = x;}));
+        activeObj.GetComponent<Toggle>().onValueChanged
+            .AddListener(new Action<bool>(x => { ObjectManagerPatch.ShouldFollowRot = x; }));
         activeObj.Find("content/text").GetComponent<TextMeshProUGUI>().text = "Rotate";
 
         //setting up the tooltip
         tooltip = activeObj.GetComponent<TooltipTrigger>();
         tooltip.keyCodeTips.Clear();
         tooltip.title = "Follow Rotation";
-        tooltip.description = "if an quick spawned prefab should copy the rotation of the current selection \nDoes not work when the Camera toggle is ON";
+        tooltip.description =
+            "if an quick spawned prefab should copy the rotation of the current selection \nDoes not work when the Camera toggle is ON";
         activeObj.SetSiblingIndex(3);
-        
+
         //the Spawn on Camera toggle
         activeObj = Object.Instantiate(active, settings);
         activeObj.GetComponent<Toggle>().Set(false);
-        activeObj.GetComponent<Toggle>().onValueChanged.AddListener(new Action<bool>(x => {ObjectManagerPatch.ShouldSpawnOnCamera = x;}));
+        activeObj.GetComponent<Toggle>().onValueChanged.AddListener(new Action<bool>(x =>
+        {
+            ObjectManagerPatch.ShouldSpawnOnCamera = x;
+        }));
         activeObj.Find("content/text").GetComponent<TextMeshProUGUI>().text = "Camera";
 
         //setting up the tooltip
@@ -158,5 +191,60 @@ public class QsEditorPatch
         tooltip.title = "Spawn on Camera";
         tooltip.description = "if an quick spawned prefab should spawn on the center of the camera";
         activeObj.SetSiblingIndex(4);
+        
+        //spawn the parent offset tab
+        activeObj = Object.Instantiate(offset, settings);
+        activeObj.Find("bold-text").GetComponent<TextMeshProUGUI>().text = "Spawn Offset";
+        
+        Object.Destroy(activeObj.Find("pos_row").gameObject);
+        Object.Destroy(activeObj.Find("sca_row").gameObject);
+        
+        Object.Instantiate(position.parent.Find("value_label"), activeObj).SetSiblingIndex(2);
+        
+        Transform posOffet = Object.Instantiate(position, activeObj);
+        posOffet.SetSiblingIndex(3);
+        
+        CompLib.InitInputFieldFloat(posOffet.Find("x"),
+            _getFunc: new Func<float>(() => ObjectManagerPatch.Xoffset),
+            _setFunc:new Action<float>(x => { ObjectManagerPatch.Xoffset = x; }),
+            _refreshUINeeded:new Func<bool>(() => true),
+            _type: CompLib.InputFieldStyle.AddSub
+        );
+        CompLib.InitInputFieldFloat(posOffet.Find("y"),
+            _getFunc: new Func<float>(() => ObjectManagerPatch.Yoffset),
+            _setFunc:new Action<float>(x => { ObjectManagerPatch.Yoffset = x; }),
+            _refreshUINeeded:new Func<bool>(() => true),
+            _type: CompLib.InputFieldStyle.AddSub
+        );
+
+        Transform rotLabel = Object.Instantiate(position.parent.Find("value_label"), activeObj);
+        rotLabel.SetSiblingIndex(4);
+        rotLabel.GetChild(1).gameObject.SetActive(false);
+        rotLabel.GetComponentInChildren<TextMeshProUGUI>().text = "Rotation";
+
+        Transform rotRow = activeObj.Find("rot_row");
+        rotRow.GetChild(0).gameObject.SetActive(false);
+        rotRow.GetChild(1).gameObject.SetActive(false);
+        
+        CompLib.InitInputFieldFloat(rotRow.GetChild(2),
+            _getFunc: new Func<float>(() => ObjectManagerPatch.Zoffset),
+            _setFunc:new Action<float>(x => { ObjectManagerPatch.Zoffset = x; }),
+            _refreshUINeeded:new Func<bool>(() => true),
+            _type: CompLib.InputFieldStyle.AddSub
+        );
+        
+        activeObj.SetSiblingIndex(5);
     }
+}
+
+[HarmonyPatch(typeof(CompLib))]
+public static class test
+{
+    [HarmonyPatch(nameof(CompLib.InitInputFieldFloat))]
+    [HarmonyPrefix]
+    static void preInit(Transform _parent, CompLib.InputFieldStyle _type = CompLib.InputFieldStyle.Default)
+    {
+        
+    }
+   
 }
